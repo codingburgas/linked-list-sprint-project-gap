de3881dffa39de4e36a43c85f87946edc05bd958
NSStoyanova22 <NSStoyanova22@codingburgas.bg>
2025-03-19 00:37:51 +0200

    Load main menu

--- Gap/PL/src/Gap.cpp
+++ Gap/PL/src/Gap.cpp
@@ -1,8 +1,33 @@
-#include<iostream>
-#include <ftxui/screen/screen.hpp>  
-#include <ftxui/dom/elements.hpp>   
+#include <iostream>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+
 using namespace ftxui;
 
 int main() {
- 
+    
+    auto color_decorator = color(Color::Blue);  
+    auto bgcolor_decorator = bgcolor(Color::Yellow);  
+
+    // Create the document
+    Element document =
+        hbox({
+          text("") | border | color_decorator,  
+          text("Gap") | border | flex,
+          text("") | border | bgcolor_decorator,  
+            });
+
+    
+    auto screen = Screen::Create(
+        Dimension::Full(),
+        Dimension::Fit(document)
+    );
+
+    
+    Render(screen, document);
+
+    
+    screen.Print();
+
+    return EXIT_SUCCESS;
 }
