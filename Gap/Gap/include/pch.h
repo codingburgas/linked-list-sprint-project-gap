7a67549d8b7c1acc3502bf83435174b41389dd26
SSStefanov2223 <SSStefanov22@codingburgas.bg>
2025-03-21 00:58:55 +0200

    Write GAP with symbols in starting screen

--- Gap/Gap/include/pch.h
+++ Gap/Gap/include/pch.h
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<string>
 #include "register.h"
+#include<vector>
 #include<string>
 #include <conio.h>
 #include "../../BLL/include/register.h"
--- Gap/Gap/src/Gap.cpp
+++ Gap/Gap/src/Gap.cpp
@@ -1,24 +1,63 @@
 #include <iostream>
 #include "../include/pch.h"
 using namespace std;
+
 int main()
 {
-    int res;
-    cout << "=======================\n";
-    std::cout << "Gap\n"<<"App for history enthusiasts!\n";
-    cout << "=======================\n";
-    cout << "Press 'R' to register or 'L' to login: \n";
-    char key = _getch();  
+    //print GAP
+    const int height = 7;
+    const int width = 7;
+    const int spacing = 2;
+    const int textWidth = width * 3 + spacing * 2;
+    const int verticalHeight = 3;
+    const int horizontalWidth = 7;
 
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (i == 0 || i == height - 1 || j == 0 || (i >= 3 && j == width - 1) || (i == 3 && j >= 3))
+                std::cout << char(205);
+            else
+                std::cout << " ";
+        }
+
+        for (int s = 0; s < spacing; s++) std::cout << " ";
+
+        for (int j = 0; j < width; j++) {
+            if (j == 0 || j == width - 1 || i == 0 || i == 3)
+                std::cout << char(205);
+            else
+                std::cout << " ";
+        }
+
+        for (int s = 0; s < spacing; s++) std::cout << " ";
+
+        for (int j = 0; j < width; j++) {
+            if (j == 0 || (i == 0 || i == 3) && j < width - 1 || (j == width - 1 && i < 4))
+                std::cout << char(205);
+            else
+                std::cout << " ";
+        }
+
+        std::cout << endl;
+    }
+
+    for (int i = 0; i < textWidth; i++)
+    {
+        std::cout << "=";
+    }
+    std::cout << endl;
+    std::cout << "App for history enthusiasts!\n";
+
+    //Log in and Register logic
+    char key = _getch();
+
+    std::cout << "Type 'L' for log in and 'R' for register:";
     if (key == 'r' || key == 'R') {
         Register();
     }
     else if (key == 'l' || key == 'L') {
-
     }
     else {
-        cout << "\nInvalid key. Try again!\n";
+        std::cout << "\nInvalid key. Try again!\n";
     }
 }
-
-
